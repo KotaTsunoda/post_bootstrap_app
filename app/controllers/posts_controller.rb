4fa69da5c7e51e32fb109dd0678e5226422ab94d
Kota Tsunodae <tsunoda.0688k@outlook.jp>
2020-09-08 20:34:16 +0900

    一覧表示機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(id: :asc)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -28,5 +30,5 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :content)
   end
-  
+
 end
